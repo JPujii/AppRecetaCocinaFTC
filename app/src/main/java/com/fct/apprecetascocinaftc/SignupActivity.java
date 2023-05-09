e969d9868eef211998469912781e20944390255c
Josea <jmguardiolaabarca@gmail.com>
2023-05-09 10:48:52 +0200

    Ejemplo Crear Instancia y añadir valores

--- app/src/main/java/com/fct/apprecetascocinaftc/SignupActivity.java
+++ app/src/main/java/com/fct/apprecetascocinaftc/SignupActivity.java
@@ -4,11 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class SignupActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        // Create an instance of the Firebase Realtime Database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        // Register data to the Firebase Realtime Database
+        DatabaseReference usersRef = database.getReference("users");
+        String userId = "abc123";
+        usersRef.child(userId).child("name").setValue("John");
     }
 }
